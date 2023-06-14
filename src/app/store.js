9f3684091d6330f153832eb1b9c94dd9b319fd10
Luddendorf <luddendorf2@yahoo.com>
2023-06-14 21:06:03 +0300

    Validators for text input

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import inputTextReducer from '../components/inputText/inputTextSlice';
 import counterReducer from '../features/counter/counterSlice';
 import topbarReducer from '../features/topbar/topbarSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    inputText: inputTextReducer,
     topbar: topbarReducer
   },
 });

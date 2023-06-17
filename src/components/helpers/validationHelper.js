8be1a4cee0db1a71791854f9c5891665b5ce3130
Luddendorf <luddendorf2@yahoo.com>
2023-06-17 17:14:16 +0300

    Error messages

---
+++ src/components/helpers/validationHelper.js
@@ -0,0 +1,67 @@
+export const validators = {
+    required: {
+        validate: (input) => { return !!(input); }
+    },
+    minLength: {
+        validate: (input, param) => { return input ? input.length >= param ? true : false : false; }
+    },
+    maxLength: {
+        validate: (input, param) => { return input ? input.length <= param ? true : false : true; }
+    },
+    email: {
+        validate: (input) => {
+            return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+                String(input).toLowerCase()
+            );
+        }
+    },
+    password: {
+        validate: (input) => {
+            return String(input).match(/^(?=.*[0-9])(?=.*[A-Z])(?=.*\W).{8,20}$/);
+        }
+    }
+};
+
+export function makeValidationResult(valFunctions, userInput) {
+    let allErrors = { errors: {} };
+
+    const requiredValidator = valFunctions.find(validator => validator.name === 'required');
+    const minLengthValidator = valFunctions.find(validator => validator.name === 'minLength');
+    const maxLengthValidator = valFunctions.find(validator => validator.name === 'maxLength');
+    const emailValidator = valFunctions.find(validator => validator.name === 'email');
+    const passwordValidator = valFunctions.find(validator => validator.name === 'password');
+    if (requiredValidator) {
+        if (!requiredValidator.validate(userInput)) {
+            allErrors.errors.required = true;
+            return allErrors;
+        }
+    }
+    if (minLengthValidator) {
+        if (!minLengthValidator.validate(userInput)) {
+            allErrors.errors.minLength = true;
+            return allErrors;
+        }
+    }
+    if (maxLengthValidator) {
+        if (!maxLengthValidator.validate(userInput)) {
+            allErrors.errors.maxLength = true;
+            return allErrors;
+        }
+    }
+    if (emailValidator) {
+        if (!emailValidator.validate(userInput)) {
+            allErrors.errors.email = true;
+            return allErrors;
+        }
+    }
+    if (passwordValidator) {
+        if (!passwordValidator.validate(userInput)) {
+            allErrors.errors.password = true;
+            return allErrors;
+        }
+    }
+    if (Object.keys(allErrors.errors).length > 0) {
+        return allErrors;
+    }
+    return null;
+}
--- src/components/inputText/InputText.js
+++ src/components/inputText/InputText.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectValidationProducer, selectValidators } from './inputTextSlice';
-
+import { useState } from 'react';
+import { makeValidationResult, validators } from '../helpers/validationHelper';
 
 export function InputText(props) {
-    // const [isDisabled, setIsDisabled] = useState(false);
-    const validators = useSelector(selectValidators);
-    const validationProducer = useSelector(selectValidationProducer);
+    const [validationResult, setValidationResult] = useState(null);
+    const [hasBlurred, setHasBlurred] = useState(false);
 
     const validateField = (event) => {
         const userInput = event.target.value;
@@ -18,31 +15,30 @@ export function InputText(props) {
                 }
             };
         });
-
-        return validationProducer(valFunctions, userInput);
+        return makeValidationResult(valFunctions, userInput);
     };
 
-    const makeClassList = () => {
-        // console.log(validateEmail);
-        // validateEmail();
-        return `input-text__field ${props.disabled ? 'input-text__disabled'
-            : ''}`;
+    const makeInputResult = (event) => {
+        setValidationResult(validateField(event));
+        props.inputHandler({ input: event.target.value, validation: validationResult });
     }
 
-    // const makeInputTextPayload = (event) => {
-    //     console.log(event);
-    //     console.log(validators);
-    //     // console.log(props);
-    //     return { value: event.target.value, isValid: true, errors: [] }
-    // };
+    const makeClassList = () => {
+
+        return `input-text__field
+            ${props.disabled ? 'input-text__disabled' : ''}
+            ${validationResult && hasBlurred ? 'input-text__error' : ''}`;
+    }
+
+
 
     return (
         <div className="input-text__container">
             <input className={makeClassList()}
                 type={props.type}
                 placeholder={props.placeholder}
-                //onInput={validateField}
-                //onKeyDown={validateField}
+                onKeyUp={(event) => { makeInputResult(event) }}
+                onBlur={() => { setHasBlurred(true) }}
                 disabled={props.disabled} />
         </div>
     );
